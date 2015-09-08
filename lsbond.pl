9029750210d43220ba5e0a29f96e6a493c8a4b42
joelandman <joe.landman@gmail.com>
2015-09-07 20:49:29 -0400

    Fixed lsbond to handle newer kernels correctly Added lsbr to list bridges

--- lsbond.pl
+++ lsbond.pl
@@ -5,16 +5,19 @@ use Data::Dumper;
 
 my ($f,$dev,@devices,$cmd,$rc,@bonds,$bond,$slave_net,@slaves,$bond_nic);
 my ($path,$net,@ethtool,@ip,$ipv4,$ipv6,$line,$first,@proc);
+my ($bm,$line,$kver,$kmajor,$kminor,$krel,@kv);
 
-
+exit if (!(-e '/sys/class/net/bonding_masters'));
+($kmajor,$kminor,$krel,@kv) = split(/\./,`uname -r`);
+$kver=$kmajor+$kminor/100;
+# grab kernel revision, because things change between releases ...
 
 chomp(@devices    = `ls /sys/class/net/ `);
+open($bm,"<".'/sys/class/net/bonding_masters');
+chomp($line = <$bm>);
+close($bm);
 
-foreach $dev (@devices)
-  {
-   next if ($dev !~ /bonding_masters/i);
-   #printf "dev: %s\n",$dev;
-   chomp( @bonds = split(/\n/,`cat /sys/class/net/bonding_masters `) );
+@bonds = split(/\s+/,$line);   
 
    foreach $bond (@bonds) {
      $net->{$bond}->{state} =
@@ -26,11 +29,18 @@ foreach $dev (@devices)
      @proc = split(/\n/,`cat /proc/net/bonding/$bond`);
      foreach $line (@proc) {
        if ($line =~ /Bonding Mode:\s+(.*)/) { $net->{$bond}->{mode} = $1;}
-       if ($line =~ /Transmit Hash Policy:\s+(.*)/) { $net->{$bond}->{xmit_hash_policy} = $1;}
+       if ($kver < 3.15) {
+          if ($line =~ /Transmit Hash Policy:\s+(.*)/) { $net->{$bond}->{xmit_hash_policy} = $1;}
+         }
        if ($line =~ /MII Polling Interval(.*?):\s+(.*)/) { $net->{$bond}->{polling_interval} = $2;}
        if ($line =~ /Up Delay(.*?):\s+(.*)/) { $net->{$bond}->{up_delay} = $2;}
        if ($line =~ /Down Delay(.*?):\s+(.*)/) { $net->{$bond}->{down_delay} = $2;}
      }
+     if (($kver >= 3.15) && ($kver < 5.0)) {
+           $net->{$bond}->{xmit_hash_policy} = &get_sys_nic_data($bond,'bonding/xmit_hash_policy');
+	   $net->{$bond}->{active_slave}     = &get_sys_nic_data($bond,'bonding/active_slave');
+     }
+
      chomp(@ip = split(/\n/,`ip addr show $bond`));
 
      foreach my $line (@ip) {
@@ -43,9 +53,11 @@ foreach $dev (@devices)
          push @{$net->{$bond}->{ipv6}},{addr => $1};
        }
      }
-     chomp(@slaves    = `ls /sys/class/net/$bond`);
-     #printf "+bond: %s\n",$bond;
-     foreach $slave_net (@slaves) {
+
+     # bonding slave status has changed substantially in kver > 3.14
+     if ($kver < 3.14) {
+      chomp(@slaves    = `ls /sys/class/net/$bond`);
+      foreach $slave_net (@slaves) {
        #printf "++slave: %s\n",$slave_net;
        next if ($slave_net !~ /slave_(.*?)/) ;
        $bond_nic = $slave_net;
@@ -70,17 +82,47 @@ foreach $dev (@devices)
          my ($k,$v) = split(/:/,$line,2);
          $net->{$bond}->{nics}->{$bond_nic}->{mac}=$v
        }
+      }
      }
+    if (($kver >= 3.15) && ($kver <= 5.0)) {
+      $line = &get_sys_nic_data($bond,'bonding/slaves');
+      @slaves = split(/\s+/,$line);
+      #printf "D[%i]: slaves = %s\n",$$,join(",",@slaves);
+      foreach $slave_net (@slaves) {
+       $bond_nic = $slave_net;
+       
+       #printf "++bond nic: %s\n",$bond_nic;
+       $net->{$bond}->{nics}->{$bond_nic}->{carrier} =
+        &get_sys_nic_data($bond_nic,'carrier');
+       $net->{$bond}->{nics}->{$bond_nic}->{speed} =
+         &get_sys_nic_data($bond_nic,'speed');
+       $net->{$bond}->{nics}->{$bond_nic}->{state} =
+           &get_sys_nic_data($bond_nic,'operstate');
+       chomp(@ethtool = split(/\n/,`ethtool -i $bond_nic`));
+       foreach my $line (@ethtool) {
+         $line =~ s/\s+//g;
+         my ($k,$v) = split(/:/,$line);
+         $net->{$bond}->{nics}->{$bond_nic}->{$k}=$v
+       } 
+       chomp(@ethtool = split(/\n/,`ethtool -P $bond_nic`));
+       foreach my $line (@ethtool) {
+         $line =~ s/\s+//g;
+         my ($k,$v) = split(/:/,$line,2);
+         $net->{$bond}->{nics}->{$bond_nic}->{mac}=$v
+       } 
+      }
+
+    }
    }
-  }
 #printf "Dump: %s\n",Dumper($net);
 foreach $bond (sort keys %{$net}) {
-  printf "%s:\tmac %s\n\tstate %4s\n\tmode %s\n\txmit_hash %s\n\tpolling %s ms\n\tup_delay %s ms\n\tdown_delay %s ms\n",
+  printf "%s:\tmac %s\n\tstate %4s\n\tmode %s\n\txmit_hash %s\n\tactive slave %s\n\tpolling %s ms\n\tup_delay %s ms\n\tdown_delay %s ms\n",
     $bond,
     &format_mac($net->{$bond}->{mac},"default"),
     ($net->{$bond}->{state} =~ /up/ ? "up" : "down"),
     $net->{$bond}->{mode},
     $net->{$bond}->{xmit_hash_policy},
+    (defined($net->{$bond}->{active_slave}) ? $net->{$bond}->{active_slave} : ""),
     $net->{$bond}->{polling_interval},
     $net->{$bond}->{up_delay},
     $net->{$bond}->{down_delay}
@@ -110,16 +152,19 @@ foreach $bond (sort keys %{$net}) {
       }
       printf "%s ",$ipv6->{addr};
     }
-  printf "\n";
+  printf "\tslave nics:\n";
   foreach $slave_net (sort keys %{$net->{$bond}->{nics}}) {
-    printf "  %s: mac %s, link %s, state %4s, speed %s, driver %s\n",
+    printf "\t%s: mac %s, link %s, state %4s, speed %7s,\n\t\tdriver %s, version %s\n\t\tfirmware version %s\n",
       $slave_net,
       $net->{$bond}->{nics}->{$slave_net}->{mac},
       $net->{$bond}->{nics}->{$slave_net}->{carrier},
       $net->{$bond}->{nics}->{$slave_net}->{state},
       $net->{$bond}->{nics}->{$slave_net}->{speed},
-      $net->{$bond}->{nics}->{$slave_net}->{driver};
+      $net->{$bond}->{nics}->{$slave_net}->{driver},
+      $net->{$bond}->{nics}->{$slave_net}->{version},
+      $net->{$bond}->{nics}->{$slave_net}->{'firmware-version'};
   }
+  printf "\n\n";
 }
 
 sub get_sys_nic_data {
